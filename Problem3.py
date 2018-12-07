329b8d543db75092af9581078ff6ae9d08da273a
Aleksas <>
2018-12-07 19:44:02 +0800

    Added training values

--- Problem3.py
+++ Problem3.py
@@ -15,11 +15,11 @@ def multiple_variable_gradient_descent(X_train, y_train, X_test, y_test, learnin
             w[j] = w[j] - (1 / (2 * N)) * learning_rate * (
             (y_train - (X_train.dot(w))).T.dot(np.negative(X_train[:, j])))
 
-    #y_pred_train = X_train.dot(w)
+    y_pred_train = X_train.dot(w)
     y_pred = X_test.dot(w)
 
-    #print("Training values: Mean squared error: {0}, Regression score: {1} "
-    #      .format(mean_squared_error(y_train, y_pred_train), r2_score(y_test, y_pred_train)))
+    print("Training values: Mean squared error: {0}, Regression score: {1} "
+          .format(mean_squared_error(y_train, y_pred_train), r2_score(y_train, y_pred_train)))
 
     print("Final values: Mean squared error: {0}, Regression score: {1} "
           .format(mean_squared_error(y_test, y_pred), r2_score(y_test, y_pred)))
